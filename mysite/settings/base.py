617c98d24a31686983124026d970cfb2e491fa7d
Yariv Friedman <yrfriedman@gmail.com>
2021-10-09 18:24:46 +0200

    second commit with postgres

--- mysite/settings/base.py
+++ mysite/settings/base.py
@@ -93,8 +93,12 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'dbher',
+        'USER': 'heruser',
+        'PASSWORD': '2020',
+        'HOST': '',
+        'PORT': '5432',
     }
 }
 
