539a9d9050ac5729c6e72550b28ddccf4ff3b0cf
Yariv Friedman <yrfriedman@gmail.com>
2021-10-09 20:29:02 +0200

    first aws image

--- mysite/settings/base.py
+++ mysite/settings/base.py
@@ -163,14 +163,14 @@ MEDIA_URL = '/media/'
 
 
 # AWS settings
-AWS_STORAGE_BUCKET_NAME = os.environ.get('AWS_STORAGE_BUCKET_NAME')
-AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
-AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
-AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
-AWS_S3_FILE_OVERWRITE = False
+AWS_STORAGE_BUCKET_NAME='wagtailheroku' #os.environ.get('AWS_STORAGE_BUCKET_NAME')
+AWS_S3_CUSTOM_DOMAIN='%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
+AWS_ACCESS_KEY_ID='AKIARWL7OCYJMTYIQ5XG' #os.environ.get('AWS_ACCESS_KEY_ID')
+AWS_SECRET_ACCESS_KEY='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' #os.environ.get('AWS_SECRET_ACCESS_KEY')
+AWS_S3_FILE_OVERWRITE=False
 
-MEDIA_URL = "https://%s/" % AWS_S3_CUSTOM_DOMAIN
-DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+MEDIA_URL="https://%s/" % AWS_S3_CUSTOM_DOMAIN
+DEFAULT_FILE_STORAGE='storages.backends.s3boto3.S3Boto3Storage'
 
 # Wagtail settings
 
--- mysite/settings/production.py
+++ mysite/settings/production.py
@@ -24,8 +24,8 @@ COMPRESS_CSS_FILTERS = [
 ]
 COMPRESS_CSS_HASHING_METHOD = 'content'
 
-MEDIA_URL = "https://%s/" % AWS_S3_CUSTOM_DOMAIN
-DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+#MEDIA_URL = "https://%s/" % AWS_S3_CUSTOM_DOMAIN
+#DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
 try:
     from .local import *
